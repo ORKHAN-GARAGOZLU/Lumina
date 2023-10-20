5195a7e5ab72696f27d04b5ce66955476ae88cbf
ORKHAN-GARAGOZLU <orkhanrg@code.edu.az>
2023-10-20 23:50:52 +0400

    IV

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//Video slider
+
 var swiper = new Swiper('.swiper-container', {
   direction: 'vertical',
   slidesPerView: 1,
@@ -30,6 +32,75 @@ drawerClose.onclick = function () {
     // main.classList.remove("blur");
 
 }
+
+//search
+
+
+let siteSearch = document.querySelector(".site-search");
+
+let fixSearch = document.querySelector(".fix-search");
+
+let searchClose = document.querySelector(".fix-search .search-close");
+
+
+
+siteSearch.addEventListener("click", function(e){
+  e.preventDefault();
+  fixSearch.classList.add("show-search");
+});
+
+searchClose.onclick = function(){
+  fixSearch.classList.remove("show-search");
+}
+
+
+//Navbar Submenu
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   let submenuElements = document.querySelectorAll(".submenu-element");
+//   let submenu = document.querySelector(".submenu");
+
+//   submenuElements.forEach(function(element) {
+//     element.addEventListener("click", function(e) {
+//       e.preventDefault();
+//       submenu.style.visibility = "visible";
+      
+//     });
+//   });
+// });
+
+
+
+//select doctor
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var select = document.getElementById('Department');
+//     var teamMembers = document.querySelectorAll('.team-member')
+//     select.addEventListener('change', function () {
+//         var selectedDepartment = select.value
+        
+//         teamMembers.forEach(function (member) {
+//             member.style.display = 'none';
+//         })
+        
+//         teamMembers.forEach(function (member) {
+//             var department = member.querySelector('p').textContent;
+//             if (department === selectedDepartment) {
+//                 member.style.display = 'block';
+//             }
+//         });
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
 
 
 
